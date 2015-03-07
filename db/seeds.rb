8254873e0ffa818bea597bb57fdad6a51e169227
Henrique Breim <henrique@breim.com.br>
2015-03-07 02:17:44 -0300

    My first commit

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+Items = Item.create([{ name: 'Moto', price: 1500 }, { name: 'Bombom Vegano', price: 3.20 }])
